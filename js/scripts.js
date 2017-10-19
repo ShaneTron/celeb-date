4547f20f68d0ab1de1eee1885c16518e98ba02cd
Kevin Babcock and Shane Robinson <shane@>
2017-10-19 13:40:08 -0700

    add error messages if part of form is left blank

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,30 @@
 $(document).ready(function() {
-}
+
+  $("#personalInfo").submit(function(event) {
+    event.preventDefault();
+    var age = parseInt($("#age").val());
+    var gender = $("#gender").val();
+    var favoriteAnimal = $("#farmAnimal").val();
+
+  if (age && gender && favoriteAnimal) {
+    if (favoriteAnimal === "horse") {
+      $("#buscemi").show();
+    } else if (gender === "female" && favoriteAnimal === "chicken" && age < 60) {
+      $("#mel").show();
+    } else {
+        $("#alone").show();
+    }
+
+
+
+  } else if (!age) {
+    alert("please enter your age")
+  } else if (!gender) {
+    alert("please enter your gender")
+  } else if (!favoriteAnimal) {
+    alert("please select your favorite animal")
+  }
+
+
+  })
+})
